91cb766fd944d5d9ab5dce1e4353cbb31c8f7603
kevinopix <kevinopix@gmail.com>
2022-11-30 11:42:33 -0600

    Datamart Application Added

--- soundscraper/settings.py
+++ soundscraper/settings.py
@@ -14,8 +14,10 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 my_env = environ.Env()
 environ.Env.read_env()
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'datamart.apps.DatamartConfig',
 ]
 
 MIDDLEWARE = [
@@ -85,10 +89,8 @@ DATABASES = {
 }
 
 
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
